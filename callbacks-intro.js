6f436e95a8c976402f7d4bbf69a97cf8280c07d2
Samantha Kania <45800528+samanthakania@users.noreply.github.com>
2019-02-07 12:20:16 -0500

    waldo for each

--- callbacks-intro.js
+++ callbacks-intro.js
@@ -1,14 +1,14 @@
+
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(element, index){
+    if (element === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
-function actionWhenFound(i) {
-  var index = i;
+function actionWhenFound(index) {
   console.log("Found Waldo at index " + index);
 }
 
